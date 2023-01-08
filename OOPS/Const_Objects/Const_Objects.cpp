3f9b1e9307be2772ee71bf7a60e6207bde80673b
Harsh <goyalh4164@gmail.com>
2023-01-08 12:15:06 +0530

    Const functions

---
+++ OOPS/Const_Objects/Const_Objects.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    /*(1)when we create const objects
+    it means that we can not change 
+    the value of data members of class 
+    using that const objects and we can not 
+    call any function untill or unless it guartees 
+    that it will not change the value of the data member
+    (2)Remember we can call only const functions from const objects
+    (3)Const functions are only those which can't change the value of the data member
+    (4)Basically functions which can't change "this" keyword properties of the object
+    
+    */
+    return 0;
+}
